9a13a19574b615a5d049d0842d679dccd72e3f88
Alex Couture-Beil <alex@mofo.ca>
2017-04-12 12:08:36 -0700

    io-stress

--- misc/io_stress_test.py
+++ misc/io_stress_test.py
@@ -1,5 +1,5 @@
-import os
-import random
+import time
+
 def write(filename,data,offset):
     try:
         f = open(filename,'r+b')
@@ -9,9 +9,32 @@ def write(filename,data,offset):
     f.write(data)
     f.close()
 
+def read(filename, size, offset):
+    with open(filename, 'r') as f:
+        f.seek(offset)
+        while size > 0:
+            x = f.read(1024)
+            size -= len(x)
+            if not x:
+                break
+
+path = '/tmp/randdata'
+size = 1024 * 1024
+times = 30000
+
 while 1:
-    ch = chr(random.randint(50,60))
-    size = random.randint(1,1024*10)
-    offset = random.randint(1,1024*1024*1024*5)
-    write('/tmp/randdata', ch * size, offset)
+
+    start = time.time()
+    print 'writting'
+    for _ in xrange(times):
+        ch = chr(random.randint(50,60))
+        offset = random.randint(1,1024*1024*1024*5)
+        write(path, ch * size, offset)
+    print 'reading'
+    for _ in xrange(times):
+        offset = random.randint(1,1024*1024*1024*5)
+        read(path, offset)
+    duration = time.time() - start
+    print 'IO task took %s seconds; sleeping for the same amount of time now' % duration
+    time.sleep(duration)
 
